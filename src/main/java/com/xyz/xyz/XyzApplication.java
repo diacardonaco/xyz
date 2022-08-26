187c4c4b6ae6d4e93fde824edb7b8ccaed55f2ec
DESKTOP-GMRKOO2\Asus <diacardonaco@gmail.com>
2022-08-26 00:43:10 -0500

    feat: add login user.

--- src/main/java/com/xyz/xyz/XyzApplication.java
+++ src/main/java/com/xyz/xyz/XyzApplication.java
@@ -1,13 +1,47 @@
 package com.xyz.xyz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class XyzApplication {
+public class XyzApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XyzApplication.class, args);
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.antMatchers("/login")
+				.permitAll()
+				.antMatchers("/**")
+				.hasAnyRole("BASICO")
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/wellcome")
+				.failureUrl("/login?error=true")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutSuccessUrl("/login?logout=true")
+				.invalidateHttpSession(true)
+				.permitAll()
+				.and()
+				.csrf()
+				.disable();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("diego").password("{noop}miclave").roles("BASICO");
 	}
 
 }
